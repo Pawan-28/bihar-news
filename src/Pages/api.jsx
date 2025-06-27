a68225ed283db6d8d419b30ed65942ef0bf94350
Pawan <kp355576@gamil.com>
2025-06-27 17:52:01 +0530

    hey

--- src/Pages/api.jsx
+++ src/Pages/api.jsx
@@ -1,21 +1,21 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const API_CONFIG = {
-    development: {
-      baseUrl: 'https://harshit-backend-18mr.onrender.com'
-    },
-    production: {
-      baseUrl: 'https://harshit-ke-kalam-se.onrender.com/'  // 🔁 yahi actual render URL daalo
-    }
-  };
+// const API_CONFIG = {
+//     development: {
+//       baseUrl: 'https://harshit-backend-18mr.onrender.com'
+//     },
+//     production: {
+//       baseUrl: 'https://harshit-ke-kalam-se.onrender.com/'  // 🔁 yahi actual render URL daalo
+//     }
+//   };
   
-  const API_URL = import.meta.env.MODE === 'production'
-    ? API_CONFIG.production.baseUrl
-    : API_CONFIG.development.baseUrl;
+//   const API_URL = import.meta.env.MODE === 'production'
+//     ? API_CONFIG.production.baseUrl
+//     : API_CONFIG.development.baseUrl;
   
-  const api = axios.create({
-    baseURL: API_URL
-  });
+//   const api = axios.create({
+//     baseURL: API_URL
+//   });
 
-  export default api;
+//   export default api;
   
